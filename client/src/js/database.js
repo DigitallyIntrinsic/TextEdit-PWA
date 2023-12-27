2b381a0e877cb9bbf91f7becd67959b1bb4c8c2c
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-12-27 13:46:19 -0600

    database code start, accept content

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,14 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+  export const putDb = async (content) => {
+    console.log("Update the database");
+  
+    // Creates a connection to the database and version we want to use
+    const textDb = await openDB("jate", 1);
+  
+    // Creates a new transaction and specify the database and data privileges
+    const tx = textDb.transaction("jate", "readwrite");
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
