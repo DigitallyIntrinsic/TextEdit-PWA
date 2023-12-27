1968d55cd161c456dbfbf7c2e1fb0d8f2174978d
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-12-27 13:51:33 -0600

    added app install event handler

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -27,5 +27,8 @@ butInstall.addEventListener("click", async () => {
   butInstall.classList.toggle("hidden", true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener("appinstalled", (event) => {});
+// Event handler for the `appinstalled` event
+window.addEventListener("appinstalled", (event) => {
+  // Clears prompt
+  window.deferredPrompt = null;
+});
